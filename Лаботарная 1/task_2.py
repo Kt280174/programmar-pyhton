372ddf8588e1432a8a311deaf40b2cac9cc4d4e3
Kt280174 <95652231+Kt280174@users.noreply.github.com>
2023-10-15 01:18:42 +0300

    Update task_2.py

--- Лаботарная 1/task_2.py
+++ Лаботарная 1/task_2.py
@@ -5,6 +5,6 @@ number_symbol = 25
 number_byte = 4
 total_byte = number_pages * number_line * number_symbol * number_byte
 volume_disk = 1.44
-volume_disk_in_byte = 1024 *  1024 * volume_disk
-count_book_in_disk = int(volume_disk_in_byte /total_byte)
+volume_disk_in_byte = 1024 * 1024 * volume_disk
+count_book_in_disk = int(volume_disk_in_byte / total_byte)
 print("Количество книг, помещающихся на дискету:", count_book_in_disk)
